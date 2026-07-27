b12ffc0655b876655ad51b27875dc124971b3cf7
Navina M <navina19jan@gmail.com>
2026-07-27 19:33:45 +0530

    Time: 5 ms (80.80%) | Memory: 45.3 MB (99.05%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,16 +1,17 @@
 class Solution {
     public boolean isPalindrome(int x) {
         int digit;
-        int dup=x;
         int rev=0;
+        int rev_num=x;
         while(x>0){
             digit=x%10;
             rev=rev*10+digit;
-            x=x/10;
+            x/=10;
         }
-        if(dup==rev){
+        if(rev_num==rev){
             return true;
         }
-        return false;
+
+       return false; 
     }
 }
